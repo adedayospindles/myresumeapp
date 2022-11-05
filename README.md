e4d9b7fef33d49c5e5aa3427ab565cf2fe8c9e67
Adedayo Agboola <kreativedayo@gmail.com>
2022-11-05 19:04:09 +0100

    Updated Resume App
    resume app updated

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# resume-app
-Resume Application using Flutter
+# My Resume App by Adedayo Agboola
+
+Description: An Application showing my resume
+
+Language: Flutter 
+
+Libraries: 
+Cupertino Icons class for iOS style icons
+
+APK Link: 
+
+Appetizer Link: 
+
+Challenges: 
+1. getting my images to ddiplay well
+2. getting the transfrom code and the scroll to work
+3. Gradle issues
+
+Features: Ability to download the resume as a pdf.
---
+++ lib/const.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+// Text colors
+const cTextHeaderColor = Color(0xff2A2A2A);
+const cTextFeatureColor = Color(0xff707070);
+const cTextLabelColor = Colors.white;
+
+// Detail for profile item
+const cProfileTopic = 'About Me';
+const cProfileFeature = "I'm Adedayo Agboola. I am a solution driven person " +
+    "and I participate actively in Tech. I have keen interest in technology, Social Media, Internet Marketing, Photography, Online shopping, mobile applications, User Interface Designs, user experience, web development, coding for kids & training.";
+const cProfileLabel = 'Profile';
+const cProfilePicture = 'assets/images/avatar.png';
+const cProfileIconImage = 'assets/images/profile.png';
+const cProfileColor = Color.fromARGB(255, 133, 113, 165);
+const cProfileBegColor = Color(0xff845EC2);
+const cProfileEndColor = Color(0xffB8A1DC);
+
+// Detail for education item
+const cEducationTopic = 'Educational Background';
+const cEducationFeature =
+    "MSc. Computer Science - University of Ibadan\n (2022 - 2023)\n\n" +
+        "Pgd. Computer Science - University of Ilorin\n (2018 - 2019)\n\n" +
+        "BSc. in Zoology - OAU, Ile-Ife \n (2003 - 2008\n";
+
+const cEducationLabel = 'Education';
+const cEducationPicture = 'assets/images/book.png';
+const cEducationIconImage = 'assets/images/education.png';
+const cEducationColor = Color(0xffFFC75F);
+const cEduBegColor = Color(0xffFFC75F);
+const cEduEndColor = Color(0xffF3DDB2);
+
+// Detail for experience item
+const cExperienceTopic = 'Work Experience';
+const cExperienceFeature =
+    "Lead Website Designer \n(April 2021 - till date) \n\n" +
+        "Manager, Web Administration \n(November 2016 - March 2019)\n\n" +
+        "Senior Wordpress/Php Developer \n(February 2015 - November 2015)\n";
+
+const cExperienceLabel = 'Experience';
+const cExperiencePicture = 'assets/images/laptop.png';
+const cExperienceIconImage = 'assets/images/experience.png';
+const cExperienceColor = Color(0xffFF5E78);
+const cExpBegColor = Color(0xffF8C8D0);
+const cExpEndColor = Color(0xffFF5E78);
+
+final cCaptionTextSize = 15.0.sp;
+const cStartingIndex = 0;
